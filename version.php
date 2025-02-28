e062f77dda33a8c263ffca2d8c890f506872c59b
Snickser <snickser@gmail.com>
2025-02-28 19:52:44 +0300

    version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025022803;
+$plugin->version   = 2025022804;
 $plugin->requires  = 2023100900;
 $plugin->component = 'paygw_bepaid';
 $plugin->release   = '0.4';
